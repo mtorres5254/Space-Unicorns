90d29ab61fa5934b667d3fb32c2d86698589e83f
mtorres5254 <47557307+mtorres5254@users.noreply.github.com>
2019-10-09 13:41:15 +0200

    los codigo

---
+++ maps/another-world-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="another-world-tileset" tilewidth="32" tileheight="32" tilecount="60" columns="15">
+ <image source="../Space Unicornios/Game/maps/Sideview Sci-Fi - Patreon Collection/Environments/another-world/PNG/layered/another-world-tileset.png" width="480" height="128"/>
+</tileset>
