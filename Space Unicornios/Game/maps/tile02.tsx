9791ab87df4721356ff291810f52cc245726d148
Ramsubito <47743770+Ramsubito@users.noreply.github.com>
2019-10-10 19:27:09 +0200

    Tileses en proceso

---
+++ Space Unicornios/Game/maps/tile02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="scifi_platformTiles_32x32" tilewidth="32" tileheight="32" tilecount="805" columns="35">
+ <image source="../../../../scifi_platformTiles_32x32.png" width="1120" height="736"/>
+</tileset>
