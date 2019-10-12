ed0c7ee14b6be5f5173dc4dab5d1e8f4780f6c72
Ramsubito <47743770+Ramsubito@users.noreply.github.com>
2019-10-12 19:03:25 +0200

    map idea, change tiles

--- Space Unicornios/Game/maps/tile02.tsx
+++ Space Unicornios/Game/maps/tile02.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="block06" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../../../tiles_v2/block06.png" trans="ff00ff" width="64" height="64"/>
+<tileset version="1.2" tiledversion="1.2.4" name="tile02" tilewidth="32" tileheight="32" tilecount="156" columns="13">
+ <image source="../../../../intbuilding7a.png" trans="ff00ff" width="432" height="396"/>
 </tileset>
