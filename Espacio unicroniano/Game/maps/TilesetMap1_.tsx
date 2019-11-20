98cca76650dfa6126c39350c5ca4c9e5f755335d
Ramsubito <47743770+Ramsubito@users.noreply.github.com>
2019-11-20 19:40:16 +0100

    r

--- Espacio unicroniano/Game/maps/TilesetMap1_.tsx
+++ Espacio unicroniano/Game/maps/TilesetMap1_.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="Tileset" tilewidth="32" tileheight="32" tilecount="88" columns="8">
+<tileset version="1.2" tiledversion="1.2.4" name="fff" tilewidth="32" tileheight="32" tilecount="88" columns="8">
  <image source="TilesetMap1.png" trans="ff00ff" width="256" height="368"/>
 </tileset>
