86b06a3aefdcec8215e741c5d4ca4ab003a4c8b7
mtorres5254 <47557307+mtorres5254@users.noreply.github.com>
2019-11-20 12:31:17 +0100

    deleted inecesary files

--- Space Unicornios/Game/maps/TilesetMap2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="TilesetMap2" tilewidth="32" tileheight="32" tilecount="60" columns="15">
- <image source="tilesetguay.png" trans="ff00ff" width="480" height="128"/>
-</tileset>
